e9a4b06461e0e7360e26fb3aa8dd6b00ee51e41b
BS <bsbalaji101005@gmail.com>
2026-02-24 11:10:43 +0530

    Update

---
+++ examportal/src/main/java/com/bsExamPortal/examportal/security/JwtUtil.java
@@ -0,0 +1,45 @@
+package com.bsExamPortal.examportal.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Date;
+
+public class JwtUtil {
+    private final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    // Generate Token
+    public String generateToken(String username){
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() +1000 * 60 * 60))
+                .signWith(SECRET_KEY)
+                .compact();
+    }
+
+    //Extract UserName
+    public String extractUsername(String token ){
+        return extractClaims(token).getSubject();
+    }
+    // Validate Token
+    public boolean validateToken(String token, String username) {
+        final String extractedUsername = extractUsername(token);
+        return extractedUsername.equals(username) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractClaims(token).getExpiration().before(new Date());
+    }
+
+    private Claims extractClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
