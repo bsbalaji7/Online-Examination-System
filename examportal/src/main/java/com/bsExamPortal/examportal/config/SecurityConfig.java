506510326ce6d1947b4458b83cabebf25a35ddd0
BS <bsbalaji101005@gmail.com>
2026-07-12 19:02:35 +0530

    Update SecurityConfig.java

--- examportal/src/main/java/com/bsExamPortal/examportal/config/SecurityConfig.java
+++ examportal/src/main/java/com/bsExamPortal/examportal/config/SecurityConfig.java
@@ -4,13 +4,17 @@ import com.bsExamPortal.examportal.security.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+@Configuration
+@EnableMethodSecurity
+@RequiredArgsConstructor
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
