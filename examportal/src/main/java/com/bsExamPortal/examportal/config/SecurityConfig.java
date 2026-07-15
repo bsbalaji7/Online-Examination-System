89c14732e88a74402ea7ec5fa37dbf1cec3590ee
BS <bsbalaji101005@gmail.com>
2026-07-15 09:45:59 +0530

    Update SecurityConfig.java

--- examportal/src/main/java/com/bsExamPortal/examportal/config/SecurityConfig.java
+++ examportal/src/main/java/com/bsExamPortal/examportal/config/SecurityConfig.java
@@ -12,10 +12,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 @Configuration
 @EnableMethodSecurity
-@RequiredArgsConstructor
-@Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
@@ -23,8 +22,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
         http
+                .cors(cors -> {})
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
