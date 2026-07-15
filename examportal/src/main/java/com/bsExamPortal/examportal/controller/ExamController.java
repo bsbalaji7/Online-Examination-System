b04cb4a27cc8aeed3c8ce8c09fa80f05f149855d
BS <bsbalaji101005@gmail.com>
2026-07-15 09:46:56 +0530

    Update ExamController.java

--- examportal/src/main/java/com/bsExamPortal/examportal/controller/ExamController.java
+++ examportal/src/main/java/com/bsExamPortal/examportal/controller/ExamController.java
@@ -3,7 +3,9 @@ package com.bsExamPortal.examportal.controller;
 import com.bsExamPortal.examportal.entity.Exam;
 import com.bsExamPortal.examportal.service.ExamService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import jakarta.validation.Valid;
 
 import java.util.List;
 
@@ -14,38 +16,59 @@ public class ExamController {
 
     private final ExamService examService;
 
+    // ==========================
+    // ADMIN ONLY
     // Create Exam
+    // ==========================
     @PostMapping
-    public Exam createExam(@RequestBody Exam exam) {
+    @PreAuthorize("hasRole('ADMIN')")
+    public Exam createExam(@Valid @RequestBody Exam exam) {
         return examService.createExam(exam);
     }
 
-    // Get All Exams
+    // ==========================
+    // ADMIN & STUDENT
+    // View All Exams
+    // ==========================
     @GetMapping
+    @PreAuthorize("hasAnyRole('ADMIN','STUDENT')")
     public List<Exam> getAllExams() {
         return examService.getAllExams();
     }
 
-    // Get Exam By ID
+    // ==========================
+    // ADMIN & STUDENT
+    // View Exam By ID
+    // ==========================
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN','STUDENT')")
     public Exam getExamById(@PathVariable Long id) {
         return examService.getExamById(id);
     }
 
-    // Delete Exam
-    @DeleteMapping("/{id}")
-    public String deleteExam(@PathVariable Long id) {
-        examService.deleteExam(id);
-        return "Exam deleted successfully";
-    }
-
+    // ==========================
+    // ADMIN ONLY
+    // Update Exam
+    // ==========================
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public Exam updateExam(
             @PathVariable Long id,
-            @RequestBody Exam exam) {
-
-        System.out.println("UPDATE API CALLED");
+            @Valid @RequestBody Exam exam) {
 
         return examService.updateExam(id, exam);
+    }
+
+    // ==========================
+    // ADMIN ONLY
+    // Delete Exam
+    // ==========================
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String deleteExam(@PathVariable Long id) {
+
+        examService.deleteExam(id);
+
+        return "Exam deleted successfully";
     }
 }
