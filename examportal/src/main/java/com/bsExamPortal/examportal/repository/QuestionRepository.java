e40d64a35d20bdbda6fb0b3212e3397570932d0f
BS <bsbalaji101005@gmail.com>
2026-07-23 08:09:58 +0530

    Create QuestionRepository.java

---
+++ examportal/src/main/java/com/bsExamPortal/examportal/repository/QuestionRepository.java
@@ -0,0 +1,12 @@
+package com.bsExamPortal.examportal.repository;
+
+import com.bsExamPortal.examportal.entity.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+
+    List<Question> findByExamId(Long examId);
+
+}
