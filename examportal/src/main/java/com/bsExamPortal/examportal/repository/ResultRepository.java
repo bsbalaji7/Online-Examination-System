c7152f80b54bdece6e555fee6ef9b23c794d4349
BS <bsbalaji101005@gmail.com>
2026-07-24 09:14:35 +0530

    Create ResultRepository.java

---
+++ examportal/src/main/java/com/bsExamPortal/examportal/repository/ResultRepository.java
@@ -0,0 +1,8 @@
+package com.bsExamPortal.examportal.repository;
+
+import com.bsExamPortal.examportal.entity.Result;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResultRepository extends JpaRepository<Result, Long> {
+
+}
