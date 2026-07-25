77a8d5806519bb581992675cce27798186958e73
BS <bsbalaji101005@gmail.com>
2026-07-25 09:53:37 +0530

    Update Exam.java

--- examportal/src/main/java/com/bsExamPortal/examportal/entity/Exam.java
+++ examportal/src/main/java/com/bsExamPortal/examportal/entity/Exam.java
@@ -1,6 +1,9 @@
 package com.bsExamPortal.examportal.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Entity
@@ -12,11 +15,17 @@ public class Exam {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Title is required")
     private String title;
 
+    @NotBlank(message = "Description is required")
     private String description;
 
+    @NotNull(message = "Duration is required")
+    @Min(value = 1, message = "Duration must be greater than 0")
     private Integer duration;
 
+    @NotNull(message = "Total marks is required")
+    @Min(value = 1, message = "Total marks must be greater than 0")
     private Integer totalMarks;
 }
