3b9acd5916d62bde48f4de8ff01bfe20c87fd2fd
BS <bsbalaji101005@gmail.com>
2026-07-28 21:48:33 +0530

    Update App.jsx

--- examportal/exam-frontend/src/App.jsx
+++ examportal/exam-frontend/src/App.jsx
@@ -2,43 +2,126 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+
 import AdminDashboard from "./pages/AdminDashboard";
 import StudentDashboard from "./pages/StudentDashboard";
+
 import ExamList from "./pages/ExamList";
 import TakeExam from "./pages/TakeExam";
 import Result from "./pages/Result";
+
 import CreateExam from "./pages/CreateExam";
 import ManageExams from "./pages/ManageExams";
 import EditExam from "./pages/EditExam";
 import ManageQuestions from "./pages/ManageQuestions";
 
+import ProtectedRoute from "./components/ProtectedRoute";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+
+        {/* PUBLIC ROUTES */}
+
         <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/student" element={<StudentDashboard />} />
-        <Route path="/exams" element={<ExamList />} />
-        <Route path="/take-exam/:examId" element={<TakeExam />} />
-        <Route path="/result" element={<Result />} />
+
         <Route
-    path="/admin/create-exam"
-    element={<CreateExam />}
-/>
-<Route
-    path="/admin/exams"
-    element={<ManageExams />}
-/>
-<Route
-    path="/admin/edit-exam/:id"
-    element={<EditExam />}
-/>
-<Route
-    path="/admin/questions"
-    element={<ManageQuestions />}
-/>
+          path="/register"
+          element={<Register />}
+        />
+
+
+        {/* =========================
+            STUDENT ROUTES
+        ========================== */}
+
+        <Route
+          path="/student"
+          element={
+            <ProtectedRoute allowedRole="STUDENT">
+              <StudentDashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/exams"
+          element={
+            <ProtectedRoute allowedRole="STUDENT">
+              <ExamList />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/take-exam/:examId"
+          element={
+            <ProtectedRoute allowedRole="STUDENT">
+              <TakeExam />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/result"
+          element={
+            <ProtectedRoute allowedRole="STUDENT">
+              <Result />
+            </ProtectedRoute>
+          }
+        />
+
+
+        {/* =========================
+            ADMIN ROUTES
+        ========================== */}
+
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute allowedRole="ADMIN">
+              <AdminDashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/admin/create-exam"
+          element={
+            <ProtectedRoute allowedRole="ADMIN">
+              <CreateExam />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/admin/exams"
+          element={
+            <ProtectedRoute allowedRole="ADMIN">
+              <ManageExams />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/admin/edit-exam/:id"
+          element={
+            <ProtectedRoute allowedRole="ADMIN">
+              <EditExam />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/admin/questions"
+          element={
+            <ProtectedRoute allowedRole="ADMIN">
+              <ManageQuestions />
+            </ProtectedRoute>
+          }
+        />
+
       </Routes>
     </BrowserRouter>
   );
