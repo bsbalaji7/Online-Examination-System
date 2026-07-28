e526b95b029337c81e9a9be211a5db2cdf42a70a
BS <bsbalaji101005@gmail.com>
2026-07-28 22:16:15 +0530

    Update JwtFilter.java

--- examportal/src/main/java/com/bsExamPortal/examportal/security/JwtFilter.java
+++ examportal/src/main/java/com/bsExamPortal/examportal/security/JwtFilter.java
@@ -24,6 +24,7 @@ public class JwtFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(
+
             HttpServletRequest request,
             HttpServletResponse response,
             FilterChain filterChain)
