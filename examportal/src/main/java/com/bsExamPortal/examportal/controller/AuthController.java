66c4dea16b80783bc800c74e993a6a70332d5bb4
BS <bsbalaji101005@gmail.com>
2026-07-28 22:16:36 +0530

    Update AuthController.java

--- examportal/src/main/java/com/bsExamPortal/examportal/controller/AuthController.java
+++ examportal/src/main/java/com/bsExamPortal/examportal/controller/AuthController.java
@@ -1,37 +1,41 @@
 package com.bsExamPortal.examportal.controller;
 
+import com.bsExamPortal.examportal.dto.LoginRequest;
+import com.bsExamPortal.examportal.dto.LoginResponse;
 import com.bsExamPortal.examportal.dto.RegisterRequest;
 import com.bsExamPortal.examportal.entity.User;
+import com.bsExamPortal.examportal.security.JwtUtil;
 import com.bsExamPortal.examportal.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.bsExamPortal.examportal.dto.LoginRequest;
-import com.bsExamPortal.examportal.security.JwtUtil;
-
 import java.util.List;
-
 
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
+
     private final JwtUtil jwtUtil;
     private final UserService userService;
+
     @PostMapping("/register")
     public ResponseEntity<User> register(@RequestBody RegisterRequest request) {
+
         User user = userService.registerUser(
                 request.getName(),
                 request.getEmail(),
                 request.getPassword(),
                 request.getRole()
         );
+
         return ResponseEntity.ok(user);
     }
+
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+    public ResponseEntity<LoginResponse> login(
+            @RequestBody LoginRequest request) {
 
         User user = userService.loginUser(
                 request.getEmail(),
@@ -40,8 +44,17 @@ public class AuthController {
 
         String token = jwtUtil.generateToken(user.getEmail());
 
-        return ResponseEntity.ok(token);
+        LoginResponse response = new LoginResponse(
+                token,
+                user.getId(),
+                user.getName(),
+                user.getEmail(),
+                user.getRole().getRoleName()
+        );
+
+        return ResponseEntity.ok(response);
     }
+
     @GetMapping("/users")
     public List<User> getAllUsers() {
         return userService.getAllUsers();
